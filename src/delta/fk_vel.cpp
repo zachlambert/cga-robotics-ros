a41bbc86f179c30c7a032db4f467789f7a266245
Zach Lambert <zachalambert@gmail.com>
2021-02-02 22:28:13 +0000

    Added fk_vel node. Added twist visualisation markers.

---
+++ src/delta/fk_vel.cpp
@@ -0,0 +1,91 @@
+#include <sstream>
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#include "cbot/delta.h"
+namespace cbot { using namespace cga_impl; }
+
+#include "cbot/conversions.h"
+
+class Node {
+public:
+    Node(ros::NodeHandle &n, cbot::Delta::Config config): delta(config)
+    {
+        theta_vel_sub = n.subscribe(
+            "joint_velocities", 1, &Node::joint_velocities_callback, this
+        );
+
+        loop_timer = n.createTimer(ros::Duration(1.0/20), &Node::loop, this);
+
+        std::stringstream ss;
+        std::string topic;
+        for (int i = 0; i < 3; i++) {
+            ss.str("");
+            ss << "theta_" << (i+1) << "_controller/command";
+            topic = ss.str();
+            ss.clear();
+            theta_pub[i] = n.advertise<std_msgs::Float64>(topic, 1);
+        }
+
+        ee_twist_pub = n.advertise<geometry_msgs::TwistStamped>("ee_twist", 1);
+
+        std::fill_n(joints_pos.theta, 3, 0);
+    }
+
+    void loop(const ros::TimerEvent &timer)
+    {
+        double dt = (timer.current_real - timer.last_real).toSec();
+        for (int i = 0; i < 3; i++) {
+            joints_pos.theta[i] += joints_vel.theta[i]*dt;
+            std_msgs::Float64 msg;
+            msg.data = joints_pos.theta[i];
+            theta_pub[i].publish(msg);
+        }
+
+        cbot::Twist twist;
+        delta.fk_twist(joints_pos, joints_vel, twist);
+        geometry_msgs::TwistStamped twist_msg;
+        twist_msg.twist = cbot::to_msg(twist);
+        twist_msg.header.frame_id = "base";
+        twist_msg.header.stamp = ros::Time::now();
+        ee_twist_pub.publish(twist_msg);
+    }
+
+    void joint_velocities_callback(const std_msgs::Float64MultiArray &joint_velocities)
+    {
+        std::size_t n = 3;
+        if (joint_velocities.data.size() < 3) { n = joint_velocities.data.size(); }
+        for (int i = 0; i < n; i++) {
+            joints_vel.theta[i] = joint_velocities.data[i];
+        }
+    }
+
+private:
+    cbot::Delta delta;
+    ros::Subscriber theta_vel_sub;
+    cbot::Delta::Joints joints_pos, joints_vel;
+    ros::Timer loop_timer;
+
+    ros::Publisher theta_pub[3];
+    ros::Publisher ee_twist_pub;
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "delta_fk_vel");
+    ros::NodeHandle n;
+
+    cbot::Delta::Config config;
+    ros::NodeHandle n_local("~");
+    n_local.getParam("base_radius", config.r_base);
+    n_local.getParam("ee_radius", config.r_ee);
+    n_local.getParam("upper_length", config.l_upper);
+    n_local.getParam("lower_length", config.l_lower);
+
+    Node node(n, config);
+    ros::spin();
+}
---
+++ src/twist_marker.cpp
@@ -0,0 +1,101 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Quaternion.h>
+#include <visualization_msgs/Marker.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+void configure_arrow(
+    const geometry_msgs::PoseStamped pose,
+    const geometry_msgs::Vector3 vector,
+    visualization_msgs::Marker &arrow)
+{
+    arrow.header.frame_id = "base";
+    arrow.header.stamp = ros::Time::now();
+    arrow.ns = "";
+    arrow.id = 0;
+    arrow.type = visualization_msgs::Marker::ARROW;
+    arrow.pose.position = pose.pose.position;
+
+    // Quaternion rotates from [1 0 0] to vector;
+    Eigen::Vector3d a(1, 0, 0);
+    Eigen::Vector3d b(vector.x, vector.y, vector.z);
+    auto quat = Eigen::Quaterniond::FromTwoVectors(a, b.normalized());
+    arrow.pose.orientation.x = quat.x();
+    arrow.pose.orientation.y = quat.y();
+    arrow.pose.orientation.z = quat.z();
+    arrow.pose.orientation.w = quat.w();
+
+    arrow.scale.x = b.norm();
+    arrow.scale.y = 0.01;
+    arrow.scale.z = 0.01;
+}
+
+class Node {
+public:
+    Node(ros::NodeHandle &n)
+    {
+        ee_pose_sub = n.subscribe(
+            "ee_pose", 1, &Node::ee_pose_callback, this
+        );
+        ee_twist_sub = n.subscribe(
+            "ee_twist", 1, &Node::ee_twist_callback, this
+        );
+
+        loop_timer = n.createTimer(ros::Duration(1.0/20), &Node::loop, this);
+
+        linear_pub = n.advertise<visualization_msgs::Marker>(
+            "ee_twist_linear", 1
+        );
+        angular_pub = n.advertise<visualization_msgs::Marker>(
+            "ee_twist_angular", 1
+        );
+    }
+
+    void ee_pose_callback(const geometry_msgs::PoseStamped &ee_pose)
+    {
+        this->ee_pose = ee_pose;
+    }
+
+    void ee_twist_callback(const geometry_msgs::TwistStamped &ee_twist)
+    {
+        this->ee_twist = ee_twist;
+    }
+
+    void loop(const ros::TimerEvent &timer)
+    {
+        visualization_msgs::Marker linear;
+        configure_arrow(ee_pose, ee_twist.twist.linear, linear);
+        linear.color.a = 1.0;
+        linear.color.r = 0;
+        linear.color.g = 1.0;
+        linear.color.b = 1.0;
+        linear_pub.publish(linear);
+
+        visualization_msgs::Marker angular;
+        configure_arrow(ee_pose, ee_twist.twist.angular, angular);
+        angular.color.a = 1.0;
+        angular.color.r = 1.0;
+        angular.color.g = 1.0;
+        angular.color.b = 0;
+        angular_pub.publish(angular);
+    }
+
+
+private:
+    ros::Subscriber ee_pose_sub, ee_twist_sub;
+    geometry_msgs::PoseStamped ee_pose;
+    geometry_msgs::TwistStamped ee_twist;
+    ros::Publisher linear_pub, angular_pub;
+    ros::Timer loop_timer;
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "twist_marker");
+    ros::NodeHandle n;
+    Node node(n);
+    ros::spin();
+}
