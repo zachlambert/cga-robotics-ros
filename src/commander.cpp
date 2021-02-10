9b982b1a63295d29ce653e3df1677267b69f7a66
Zach Lambert <zachalambert@gmail.com>
2021-02-10 15:49:19 +0000

    More work on commander. Started adding action server for trajectory execution.

--- src/commander.cpp
+++ src/commander.cpp
@@ -4,6 +4,7 @@
 #include <geometry_msgs/TwistStamped.h>
 #include <cga_robotics_ros/PoseCommand.h>
 #include <cga_robotics_ros/VelocityCommand.h>
+#include "joystick_listener.h"
 
 class Node {
 public:
@@ -11,7 +12,8 @@ public:
         std::string gripper_joint;
     };
 
-    Node(ros::NodeHandle &n, Config config): config(config)
+    Node(ros::NodeHandle &n, Config config):
+        config(config), joystick_listener(n)
     {
         ee_pose_sub = n.subscribe(
             "ee_pose", 1, &Node::ee_pose_callback, this
@@ -22,6 +24,37 @@ public:
         ee_twist_pub = n.advertise<geometry_msgs::TwistStamped>(
             "ee_twist", 1
         );
+
+        loop_timer = n.createTimer(
+            ros::Duration(1.0/20),
+            &Node::loop,
+            this
+        );
+    }
+
+    void loop(const ros::TimerEvent &timer)
+    {
+        if (joystick_listener.query_button_state(JoyButton::Y) == JoyButtonState::PRESSED) {
+
+        }
+
+        ee_twist.header.stamp = ros::Time::now();
+
+        // Set linear velocity
+        ee_twist.twist.linear.x =
+            joystick_listener.query_axis(JoyAxis::LEFT_VERTICAL);
+        ee_twist.twist.linear.y =
+            joystick_listener.query_axis(JoyAxis::LEFT_HORIZONTAL);
+        ee_twist.twist.linear.z =
+            joystick_listener.query_axis(JoyAxis::RT)
+            - joystick_listener.query_axis(JoyAxis::LT);
+
+        // Don't bother with angular velocity for now, since delta doesn't use
+        // it. Later on, set angular velocity using euler velocities.
+        // Controller is responsible for the coordinate transformation to
+        // angular velocity.
+
+        ee_twist_pub.publish(ee_twist);
     }
 
     void ee_pose_callback(const geometry_msgs::PoseStamped pose_msg)
@@ -41,12 +74,15 @@ public:
 
 private:
     Config config;
+    JoystickListener joystick_listener;
 
     ros::Subscriber joint_states_sub;
     ros::Subscriber ee_pose_sub;
     ros::Publisher ee_twist_pub;
+    ros::Timer loop_timer;
 
     geometry_msgs::PoseStamped ee_pose;
+    geometry_msgs::TwistStamped ee_twist;
     double gripper_angle;
 };
 
