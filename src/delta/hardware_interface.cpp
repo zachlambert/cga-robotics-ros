a175088edce910ca0dee8789bfe2a5e21376640d
Zach Lambert <zachalambert@gmail.com>
2021-05-25 18:41:46 +0100

    Delta hw working

--- src/delta/hardware_interface.cpp
+++ src/delta/hardware_interface.cpp
@@ -2,11 +2,59 @@
 #include <vector>
 #include <stdint.h>
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <stdio.h>
+
 #include <ros/callback_queue.h>
 #include <controller_manager/controller_manager.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
+
+int get_fd()
+{
+    struct termios serial;
+
+    char device[15] = "/dev/ttyACM0";
+
+    int fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY | O_FSYNC);
+
+    if (fd == -1) {
+        std::cerr << "Failed to open serial device" << std::endl;
+        return 1;
+    }
+    if (tcgetattr(fd, &serial) < 0) {
+        std::cerr << "Failed to get configuration" << std::endl;
+        return 1;
+    }
+
+    serial.c_iflag = 0;
+    serial.c_oflag = 0;
+    serial.c_lflag = 0;
+    serial.c_cflag = 0;
+    serial.c_cc[VMIN] = 0;
+    serial.c_cc[VTIME] = 0;
+    serial.c_cflag = B9600 | CS8 | CREAD;
+
+    tcsetattr(fd, TCSANOW, &serial);
+
+    return fd;
+}
+
+void write_pos(int fd, int i, double angle)
+{
+    double degrees = angle*180/M_PI + 22;
+    if (degrees < 0) degrees = 0;
+    if (degrees > 90) degrees = 90;
+    static uint16_t signatures[] = {111, 222, 333, 444, 555};
+    uint16_t cmd = (5000.0*10.0/513) * degrees;
+    uint32_t packet = cmd << 16 | signatures[i];
+    write(fd, &packet, 4);
+}
 
 class Hardware: public hardware_interface::RobotHW {
 public:
@@ -28,6 +76,8 @@ public:
         }
         registerInterface(&state_interface);
         registerInterface(&cmd_interface);
+
+        fd = get_fd();
     }
 
     ~Hardware()
@@ -42,10 +92,7 @@ public:
     void write()
     {
         for (std::size_t i = 0; i < cmd.size(); i++) {
-            double angle = cmd[i];
-            while (angle < 0) angle += 2*M_PI;
-            while (angle > 2*M_PI) angle -= 2*M_PI;
-            uint16_t cmd = (angle/(2*M_PI))*65536;
+            write_pos(fd, i, cmd[i]);
         }
     }
 
@@ -55,6 +102,8 @@ private:
 
     std::vector<std::string> names;
     std::vector<double> pos, vel, eff, cmd;
+
+    int fd;
 };
 
 class Node {
@@ -62,7 +111,7 @@ public:
     Node(ros::NodeHandle& n): hw(n), cm(&hw, n)
     {
         loop_timer = n.createTimer(
-            ros::Duration(1.0/50),
+            ros::Duration(1.0/25),
             &Node::loop,
             this
         );
